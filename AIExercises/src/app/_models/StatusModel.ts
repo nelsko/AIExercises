c08172b729389fde2626b80f014ed3437f1b665c
Nils Oelrich <n.oelrich@intra.i2group.de>
2020-04-05 17:25:54 +0200

    chartjs and navbar

---
+++ AIExercises/src/app/_models/StatusModel.ts
@@ -0,0 +1,5 @@
+export class StatusModel {
+  StudentsRegisteredCount: number;
+  HomeworkNeedsCorrectionCount: number;
+  TestsNeedCorrectionCount: number;
+}
--- AIExercises/src/app/dashboard/dashboard.component.ts
+++ AIExercises/src/app/dashboard/dashboard.component.ts
@@ -1,34 +1,138 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
+import { StatusModel } from '../_models/StatusModel';
 @Component({
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  ngOnInit(){
-    //new Chart(document.getElementById("canvas"), {
-    //  type: 'bar',
-    //  data: {
-    //    labels: ["1900", "1950", "1999", "2050"],
-    //    datasets: [
-    //      {
-    //        label: "Africa",
-    //        backgroundColor: "#3e95cd",
-    //        data: [133, 221, 783, 2478]
-    //      }, {
-    //        label: "Europe",
-    //        backgroundColor: "#8e5ea2",
-    //        data: [408, 547, 675, 734]
-    //      }
-    //    ]
-    //  },
-    //  options: {
-    //    title: {
-    //      display: true,
-    //      text: 'Population growth (millions)'
-    //    }
-    //  }
-    //});
-}
- 
+export class DashboardComponent implements OnInit, AfterViewInit {
+  Stats: StatusModel = new StatusModel();
+  loading: boolean;
+  TestsHomeworksOverTime = [];
+  GradeDistribution = [];
+  AverageTimeSpentPerTest = [];
+  SatisfactionOfStudents = [];
+  ngAfterViewInit(): void {
+  }
+  ngOnInit() {
+    //this.loading = true;
+    setTimeout(() => {
+      this.Stats.HomeworkNeedsCorrectionCount = 10;
+      this.Stats.StudentsRegisteredCount = 20;
+      this.Stats.TestsNeedCorrectionCount = 30;
+      this.TestsHomeworksOverTime = new Chart('TestsHomeworksOverTime', {
+        type: 'line',
+        data: {
+          labels: [new Date(2020, 4, 3), new Date(2020, 4, 4), new Date(2020, 4, 5)],
+          datasets: [
+            {
+              label: "Worksheets",
+              backgroundColor: "#3e95cd",
+              data: [20, 30, 40]
+            }, {
+              label: "Exercises",
+              backgroundColor: "#8e5ea2",
+              data: [10, 40, 20]
+            }
+          ]
+        },
+
+        options: {
+          scales: {
+            xAxes: [{
+              type: 'time',
+              time: {
+                displayFormats: {
+                  day: 'MMM YYYY'
+                }
+              }
+            }]
+          },
+          maintainAspectRatio: false,
+          responsive: true,
+
+
+        }
+      });
+      this.GradeDistribution = new Chart('GradeDistribution', {
+        type: 'bar',
+        data: {
+          labels: ["1", "2", "3", "4", "5", "6"],
+          datasets: [
+            {
+              label: "Grade 5",
+              backgroundColor: "#3e95cd",
+              data: [10, 20, 30, 40, 50, 60]
+            }, {
+              label: "Grade 6",
+              backgroundColor: "#8e5ea2",
+              data: [20, 10, 15, 30, 20, 30]
+            },
+            {
+              label: "Grade 7",
+              backgroundColor: "#36ebcd",
+              data: [40, 30, 20, 10, 60, 20]
+            },
+            {
+              label: "Grade 8",
+              backgroundColor: "#d9eb36",
+              data: [30, 50, 40, 70, 80, 10]
+            }, {
+              label: "Grade 9",
+              backgroundColor: "#eb3636",
+              data: [40, 30, 80, 10, 25, 55]
+            }
+          ]
+        },
+        options: {
+          legend: {
+            position: 'right'
+          }
+        }
+      });
+      this.AverageTimeSpentPerTest = new Chart('AverageTimeSpentPerTest', {
+        type: 'doughnut',
+        data: {
+          labels: ["Math test", "English test"],
+          datasets: [
+            {
+              data: [100, 120],
+              backgroundColor: [
+                "#FF6384",
+                "#36A2EB"
+              ],
+            }
+          ]
+        },
+        options: {
+          rotation: 1 * Math.PI,
+          circumference: 1 * Math.PI
+        }
+      });
+      this.SatisfactionOfStudents = new Chart('SatisfactionOfStudents', {
+        type: 'doughnut',
+        data: {
+          labels: ["Mr. Smith", "Mrs. Johansson", "Mr. Andersson", "Mrs. Karlsson"],
+          datasets: [
+            {
+              backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#36ebcd",
+                "#d9eb36"
+              ],
+              data: [8, 9, 7, 10]
+            }
+          ]
+        },
+        options: {
+          rotation: 1 * Math.PI,
+          circumference: 1 * Math.PI
+        }
+      });
+      this.loading = false;
+    }, 300);
+
+  }
+
 }
